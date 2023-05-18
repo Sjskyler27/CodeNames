f4dcf7c1e32bb14953059738d9384483c583554f
Skyler Simpson <sjskyler27@gmail.com>
2023-05-18 12:38:34 -0600

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        game: resolve(__dirname, "src/index.html"),
-        answer: resolve(__dirname, "src/solution/index.html"),
-        instructions: resolve(__dirname, "src/solution/index.html")
+        game: resolve(__dirname, "src/game/index.html"),
+        solution: resolve(__dirname, "src/solution/index.html"),
+        instructions: resolve(__dirname, "src/instructions/index.html")
       },
     },
   },
