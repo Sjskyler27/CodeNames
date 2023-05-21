bc39ee854623656fbb9d329917a9a3abc626ae47
Skyler Simpson <sjskyler27@gmail.com>
2023-05-20 18:51:19 -0600

    fix path

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,8 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         game: resolve(__dirname, "src/game/index.html"),
         solution: resolve(__dirname, "src/solution/index.html"),
-        instructions: resolve(__dirname, "src/instructions/index.html"),
-        // currentgames: resolve(__dirname, "src/currentGames/index.html")
+        currentgames: resolve(__dirname, "src/currentGames/index.html"),
+        instructions: resolve(__dirname, "src/instructions/index.html")
       },
     },
   },
