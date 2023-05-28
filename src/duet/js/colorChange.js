22f65a13c343ed0f292ec63d690b4818960a73e7
Skyler Simpson <sjskyler27@gmail.com>
2023-05-28 16:08:03 -0600

    fixed color change for solution page and fixed some paths, Player 1 and player 2 can now do checks.

--- src/duet/js/colorChange.js
+++ src/duet/js/colorChange.js
@@ -13,53 +13,39 @@ export class ColorChange{
         this.list = list;
         // console.log(solution);
     }
-    change(id, element){
+    change(id,element){
         //im sure there is a quiker way but this is good for now
         // check if the id passed matches a word in a given list and return its color.
-        if(this.solution.Black.indexOf(id)>=0){
-            console.log("changing to black");
-            element.style.backgroundColor = 'black';
-            element.style.color = 'white';
-        }
-        if(this.solution.Yellow.indexOf(id)>=0){
-            console.log("changing to Yellow");
-            element.style.backgroundColor = '#F1D302';
-            element.style.color = 'black';
-        }
-        if(this.solution.Player1.indexOf(id)>=0){
-            // get the first player
-            if(this.solution.firstPlayer == "red"){
-                console.log("changing to red");
-                element.style.backgroundColor = '#C1292E';
+        //make an unclick feature
+        if(element.style.backgroundColor != '#dbd4d3'){
+            if(this.solution.Black.indexOf(id)>=0){
+                console.log("changing to black");
+                element.style.backgroundColor = 'black';
                 element.style.color = 'white';
-            } else{
-                console.log("changing to blue");
-                element.style.backgroundColor = '#235789';
+            }
+            if(this.solution.Yellow.indexOf(id)>=0){
+                console.log("changing to Yellow");
+                element.style.backgroundColor = '#F1D302';
+                element.style.color = 'black';
+            }
+            if(this.solution.Green.indexOf(id)>=0){
+                console.log("changing to Green");
+                element.style.backgroundColor = '#1f8c0a';
                 element.style.color = 'white';
             }
         }
-        if(this.solution.Player2.indexOf(id)>=0){
-            if(this.solution.firstPlayer == "blue"){
-                console.log("changing to red");
-                element.style.backgroundColor = '#C1292E';
-                element.style.color = 'white';
-            } else{
-                console.log("changing to blue");
-                element.style.backgroundColor = '#235789';
-                element.style.color = 'white';
-            }
+        else{
+            console.log("changing to white");
+                element.style.backgroundColor = '#dbd4d3';
+                element.style.color = 'black';
         }
     }
 
     // initialize colorChange event, wait for a card to be clicked
     async init() {
         await new Promise(resolve => setTimeout(resolve, 500)); // Delay for .5 second to really ensure that everything is ready
-      
-        this.solution.Player1.forEach(id => {
-          let element = document.getElementById(id);
-          element.addEventListener('click', () => this.change(id, element)); 
-        });
-        this.solution.Player2.forEach(id => {
+        console.log(this.solution);
+        this.solution.Green.forEach(id => {
           let element = document.getElementById(id);
           element.addEventListener('click', () => this.change(id, element)); 
         });
@@ -72,4 +58,37 @@ export class ColorChange{
           element.addEventListener('click', () => this.change(id, element)); 
         });
       }
+    //  not neccessary, pass dif solution
+    //   async initPlayer1() {
+    //     await new Promise(resolve => setTimeout(resolve, 500)); // Delay for .5 second to really ensure that everything is ready
+        
+    //     this.solution.player1Solution.Green.forEach(id => {
+    //       let element = document.getElementById(id);
+    //       element.addEventListener('click', () => this.change(id, element)); 
+    //     });
+    //     this.solution.player1Solution.Yellow.forEach(id => {
+    //       let element = document.getElementById(id);
+    //       element.addEventListener('click', () => this.change(id, element)); 
+    //     });
+    //     this.solution.player1Solution.Black.forEach(id => {
+    //       let element = document.getElementById(id);
+    //       element.addEventListener('click', () => this.change(id, element)); 
+    //     });
+    //   }
+    //   async initPlayer2() {
+    //     await new Promise(resolve => setTimeout(resolve, 500)); // Delay for .5 second to really ensure that everything is ready
+        
+    //     this.solution.player2Solution.Green.forEach(id => {
+    //       let element = document.getElementById(id);
+    //       element.addEventListener('click', () => this.change(id, element)); 
+    //     });
+    //     this.solution.player2Solution.Yellow.forEach(id => {
+    //       let element = document.getElementById(id);
+    //       element.addEventListener('click', () => this.change(id, element)); 
+    //     });
+    //     this.solution.player2Solution.Black.forEach(id => {
+    //       let element = document.getElementById(id);
+    //       element.addEventListener('click', () => this.change(id, element)); 
+    //     });
+    //   }
     }
--- src/duet/js/game.js
+++ src/duet/js/game.js
@@ -9,14 +9,44 @@ async function init(){
   addToggle();
     const solution = new Solution();
     await solution.getSolution();
-    
     displayWords("wordGrid",solution.returnList());
-    const colorChange = new ColorChange(solution.returnSolution(), solution.returnList());
-    // every once in a while it seems this function gets called before the program is ready
-    colorChange.init();
 }
 init();
 upperInput();
+
+
+// logic for changing player
+async function playerColors(player, element) {
+  const solution = new Solution();
+  await solution.getSolution();
+
+  let colorChange;
+  if (player === 1) {
+    colorChange = new ColorChange(solution.returnSolution().player1Solution, solution.returnList());
+  } else {
+    colorChange = new ColorChange(solution.returnSolution().player2Solution, solution.returnList());
+  }
+
+   // Change color of button that was clicked to yellow and the other to green
+   element.style.backgroundColor = "#F1D302";
+   const otherElement = (player === 1) ? document.getElementById("player2") : document.getElementById("player1");
+   otherElement.style.backgroundColor = "#1f8c0a";
+
+   colorChange.init();
+}
+
+const player1Element = document.getElementById("player1");
+const player2Element = document.getElementById("player2");
+
+player1Element.addEventListener("click", function() {
+  playerColors(1, player1Element);
+});
+
+player2Element.addEventListener("click", function() {
+  playerColors(2, player2Element);
+});
+
+
 
 document.getElementById('solution_button').href = `../solution/index.html?code=${getParam("code")}`;
 
